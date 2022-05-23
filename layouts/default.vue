2918162af1251046ee47dc353f22c8ac06c758ab
홍세준 <juny3170@secuchart.com>
2022-05-23 20:30:40 +0900

    Nuxt Data 호출 방식과 API 연동 공부 완료

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+        <h1>{{ $route.name }}Page</h1>
+
+        <NuxtLink to="/">Home page</NuxtLink>
+        <NuxtLink to="/main">Main page</NuxtLink>
+        <NuxtLink to="/product">Product page</NuxtLink>
+    </header>
+
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>주니하랑의 Nuxt.js 여행기 Home</h1>
+    <P>Nuxt.js Started 😃</P>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-}
+export default { }
 </script>
